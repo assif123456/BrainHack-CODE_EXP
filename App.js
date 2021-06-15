687c5d528cdd2485edd9c94c5dc7b2440541df36
eugeneyap <49102312+eugeneyap@users.noreply.github.com>
2021-06-15 15:19:09 +0800

    add icons for home chat settings

--- App.js
+++ App.js
@@ -14,7 +14,29 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            
+            //Set the icon based on which route it is (name of the tab)
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "Chat") {
+              iconName = focused ? "envelope" : "envelope-o";
+            } else if (route.name === "Settings") {
+              iconName = "cog";
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Chat" component={ChatScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
