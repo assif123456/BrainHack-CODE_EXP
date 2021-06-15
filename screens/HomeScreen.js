4f956289ddc19ac90778042d0da3bb9c2e9878e1
jw-rgb <85917441+jw-rgb@users.noreply.github.com>
2021-06-15 16:54:18 +0800

    halfway

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet , Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Linking } from 'react-native';
 
 export default function HomeScreen() {
   return (
@@ -9,6 +10,44 @@ export default function HomeScreen() {
       <Text style={style.header}>Home</Text>
       <Text style ={style.secondline}>Good Morning (name)!</Text>
       <Text style={style.thirdline}>How are you feeling today?</Text>
+
+      <View style = {style.buttonStyle}>
+      <Button
+        title="Angry"
+      //  onPress={() => navigation.navigate("Don't Get Scammed")}
+      />
+       
+      <Button
+        title="Sad"
+      //  onPress={() => navigation.navigate("Don't Get Scammed")}
+      />
+      <Button
+        title="Frustrated"
+      //  onPress={() => navigation.navigate("Don't Get Scammed")}
+      />
+     
+      <Button
+        title="Anxious"
+      //  onPress={() => navigation.navigate("Don't Get Scammed")}
+      />
+      <Button
+        title="Happy"
+      //  onPress={() => navigation.navigate("Don't Get Scammed")}
+      />
+      </View>
+      
+      <Text style={{color: 'blue', marginLeft:20, padding: 10,textDecorationLine: 'underline'}}
+      onPress={() => Linking.openURL('http://google.com')}>
+       Click here for a more in-depth assesment
+      </Text>
+      <View
+      style={{
+         borderBottomColor: 'black',
+         borderBottomWidth: 1,
+         margin: 20,
+      }}
+      />
+      <Text style = {style.text}>Quote of the Day!</Text>
     </View>
   );
 }
@@ -22,7 +61,6 @@ const style = StyleSheet.create({
   header: {
     fontSize: 40,
     fontWeight: "bold",
-    fontFamily: "Arial",
     alignItems: "flex-start",
     justifyContent: "flex-start",
     marginLeft:20,
@@ -40,4 +78,20 @@ const style = StyleSheet.create({
     padding: 10,
     marginLeft: 20,
   },
+  buttonStyle:{
+    alignItems: "center",
+    flexDirection:"row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+
+  text:{
+    marginLeft:20,
+    padding:10,
+    fontSize:20,
+  }
+
+
 });
