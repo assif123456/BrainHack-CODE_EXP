452567f43e65e70be72c76714759ebf7a031cb64
assif123456 <43307329+assif123456@users.noreply.github.com>
2021-06-15 17:40:05 +0800

    added buttons

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,12 +1,54 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import {createStackNavigator} from "@react-navigation/stack";
+import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-
- function ChatScreen() {
+function ChatScreen() {
   return (
     <View style={style.container}>
-      <Text>Chat!</Text>
+      <TouchableOpacity style={style.button}>
+        <Image
+          style={style.butonImage}
+          source={{
+            uri: "https://cdn4.iconfinder.com/data/icons/charity-donation-1/128/f-02-512.png",
+          }}
+        />
+        <Text style={style.buttonText}>Contact a friend</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={style.button}>
+        <Image
+          style={style.butonImage}
+          source={{
+            uri: "https://pbs.twimg.com/profile_images/530544120758935552/LNxiVzJR.png",
+          }}
+        />
+        <Text style={style.buttonText}>Find a social worker</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={style.button}>
+        <Image
+          style={style.butonImage}
+          source={{
+            uri: "https://cdn.iconscout.com/icon/free/png-256/reddit-55-432536.png",
+          }}
+          resizeMode="center"
+        />
+        <Text style={style.buttonText}>Post annoymously on a forum</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={style.button}>
+        <Image
+          style={style.butonImage}
+          source={{ uri: "https://i.imgur.com/siVcRDk.png" }}
+        />
+        <Text style={style.buttonText}>Chat with bot</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={style.button}>
+        <Image
+          style={style.butonImage}
+          source={{
+            uri: "https://cdn1.iconfinder.com/data/icons/user-interface-2-glyph/32/ui_history_schedule_time-512.png",
+          }}
+        />
+        <Text style={style.buttonText}>Chat History</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,17 +56,38 @@ import {createStackNavigator} from "@react-navigation/stack";
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
   },
-  
+  button: {
+    flexDirection: "row",
+    width: "100%",
+    height: "75px",
+    margin: "10px",
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: 15,
+    borderWidth: 3,
+    borderStyle: "solid",
+    borderRadius: 50,
+    borderColor: "#bf00ff",
+  },
+  buttonText: {},
+  butonImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    margin: 10,
+  },
 });
 
-const Stack  = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function ChatStack() {
-    return(
+  return (
     <Stack.Navigator>
-        <Stack.Screen name = "Chat" component={ChatScreen}/>
-    </Stack.Navigator>)
+      <Stack.Screen name="Chat" component={ChatScreen} />
+    </Stack.Navigator>
+  );
 }
