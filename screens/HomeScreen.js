5c48880054098bd2c3461eb6a79aaf610b286378
assif123456 <43307329+assif123456@users.noreply.github.com>
2021-06-15 14:33:23 +0800

    transfered container style to style stylesheet

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,10 +5,16 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={style.container}>
       <Text>Home!</Text>
     </View>
   );
 }
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
