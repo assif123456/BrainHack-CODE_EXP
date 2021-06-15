a8f64cc594c41413bb13ed7ba0f658bd71d038bc
jw-rgb <85917441+jw-rgb@users.noreply.github.com>
2021-06-15 15:42:46 +0800

    nil

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,6 +7,8 @@ export default function HomeScreen() {
   return (
     <View style={style.container}>
       <Text style={style.header}>Home</Text>
+      <Text style ={style.secondline}>Good Morning (name)!</Text>
+      <Text style={style.thirdline}>How are you feeling today?</Text>
     </View>
   );
 }
@@ -18,10 +20,24 @@ const style = StyleSheet.create({
     // alignItems: "center",
   },
   header: {
-    fontSize: 30,
+    fontSize: 40,
     fontWeight: "bold",
     fontFamily: "Arial",
     alignItems: "flex-start",
     justifyContent: "flex-start",
+    marginLeft:20,
+    marginTop:30,
+    padding: 10,
+  },
+  secondline:{
+    fontSize: 30,
+    fontStyle: 'italic',
+    marginLeft: 30,
+  },
+  thirdline:{
+    fontSize: 20,
+    fontStyle: 'italic',
+    padding: 10,
+    marginLeft: 20,
   },
 });
