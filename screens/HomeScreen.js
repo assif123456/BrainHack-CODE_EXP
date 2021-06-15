19e64d66158defa3e34fbc9f051993078a365728
eugeneyap <49102312+eugeneyap@users.noreply.github.com>
2021-06-15 16:58:34 +0800

    added help button v1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,41 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={style.container}>
-      <Text style={style.header}>Home</Text>
+    <View
+      style={{
+        flex: 1,
+        //justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button
+        title="Help"
+        onPress={() => navigation.navigate("Contact the Police")}
+      />
     </View>
   );
+}
+
+function HomeSecondScreen() {
+  return <Text>SMS 71999 if it's not safe to call the police. When your message is successfully received by the police, an acknowledgement message will be sent back to you. https://www.police.gov.sg/SMS-71999</Text>;
+
+}
+
+const Stack = createStackNavigator();
+
+export default function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Hello" component={HomeScreen} />
+      <Stack.Screen name="Contact the Police" component={HomeSecondScreen} />
+    </Stack.Navigator>
+     );
 }
 
 const style = StyleSheet.create({
